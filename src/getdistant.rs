c0d742374ebfc15c8a8ec1198b4d1686c1338957
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-06-07 20:02:58 +0200

    finding packages is working

--- src/getdistant.rs
+++ src/getdistant.rs
@@ -3,6 +3,7 @@ use anyhow::{Result, Context};
 use std::env;
 use std::fs;
 use std::path::Path;
+use walkdir::WalkDir;
 
 pub fn getdistant(local: &Vec<(String, String, String)>) -> Result<()> {
     let url = "https://gitlab.archlinux.org/archlinux/packaging/state.git";
@@ -10,6 +11,9 @@ pub fn getdistant(local: &Vec<(String, String, String)>) -> Result<()> {
     opt.depth(1);
     let mut builder = git2::build::RepoBuilder::new();
     builder.fetch_options(opt);
+    if Path::new("/var/cache/state").exists() {
+        fs::remove_dir_all("/var/cache/state")?;
+    }
     let repo = match builder.clone(url, Path::new("/var/cache/state")) {
         Ok(repo) => repo,
         Err(e) => panic!("failed to clone: {}", e),
@@ -19,15 +23,21 @@ pub fn getdistant(local: &Vec<(String, String, String)>) -> Result<()> {
     let pkg: Vec<_> = fs::read_dir("/var/cache/state")?
         .filter_map(|e| e.ok())
         .collect();
+    let number = local.iter().count();
+    println!("Package number {}", number);
     for (name, version, release) in local.iter() {
-            let info = pkg.iter().find(|e| e.file_name().to_str().unwrap_or("") == name);
+            let info = WalkDir::new("/var/cache/state")
+                .into_iter()
+                .filter_map(|e| e.ok())
+                .find(|e| e.file_name().to_str().unwrap_or("") == name);
             if let Some(entry) = info {
-                println!("ok");
+                println!("{:?}", entry);
             } else {
                 unknown.push(name);
             }
     }
-    println!("Not found : {:?}", unknown);
+    let number = unknown.iter().count();
+    println!("Unknown packages :  {}", number);
     Ok(())
 
 }
