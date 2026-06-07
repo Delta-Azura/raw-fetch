5836d9d95e089f9dc86b48e89cb2dd9148f9be4e
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-06-07 22:13:18 +0200

    fixing bug

--- src/getconf.rs
+++ src/getconf.rs
@@ -41,14 +41,19 @@ pub fn getconf() -> Result<Vec<(String, String, String)> > {
         }
         let index = fs::read_to_string(index)?;
         let mut localdata = Vec::new();
+        let mut path = String::new();
         for i in index.lines() {
-            if i.matches("__").count() > 0 {
-                let mut parts = i.splitn(2, "__");
-                let i = parts.nth(0).context("Failed to split")?;
+            if i.matches("_").count() > 1 {
+                let pos = i.rfind('_').unwrap();
+                path = i[..pos].to_string();
+                //path = parts.next(1).context("Failed to split")?.to_string();
             } else {
-                let i = i.split_once("_").map(|(ls, _)| ls).context("Failed to split")?;
+                path = i.split_once("_").map(|(ls, _)| ls).context("Failed to split")?.to_string();
             }
-            let path = format!("{}/{}", source, &i);
+            //let i = i.split_once("_").map(|(ls, _)| ls).context("Failed to split")?;
+            let path = format!("{}/{}", source, &path);
+        
+            //let path = format!("{}/{}", source, &i);
             println!("{}", path);
             let pkgfile = fs::read_to_string(path).context("Failed to get pkgfile")?;
             let name = pkgfile.lines().find(|l| l.contains("name=")).context("Failed to get package name")?.split_once("name=").map(|(_, name)| name).context("Failed to get pkgname")?;
@@ -85,15 +90,17 @@ pub fn getconf() -> Result<Vec<(String, String, String)> > {
             }
             let index = fs::read_to_string(index)?;
             let mut localdata = Vec::new();
+            let mut path = String::new();
             for i in index.lines() {
                 if i.matches("_").count() > 1 {
-                    let mut parts = i.splitn(2, "_");
-                    let i = parts.nth(0).context("Failed to split")?;
+                    let pos = i.rfind('_').unwrap();
+                    path = i[..pos].to_string();
+                    //path = parts.next(1).context("Failed to split")?.to_string();
                 } else {
-                    let i = i.split_once("_").map(|(ls, _)| ls).context("Failed to split")?;
+                    path = i.split_once("_").map(|(ls, _)| ls).context("Failed to split")?.to_string();
                 }
-                let i = i.split_once("_").map(|(ls, _)| ls).context("Failed to split")?;
-                let path = format!("{}/{}", source, &i);
+                //let i = i.split_once("_").map(|(ls, _)| ls).context("Failed to split")?;
+                let path = format!("{}/{}", source, &path);
                 println!("{}", path);
                 let pkgfile = fs::read_to_string(path).context("Failed to get pkgfile")?;
                 let name = pkgfile.lines().find(|l| l.contains("name=")).context("Failed to get package name")?.split_once("name=").map(|(_, name)| name).context("Failed to get pkgname")?;
